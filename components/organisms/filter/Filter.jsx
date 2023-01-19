a4f011a77337ce9134f14db0501cda8c69b0d024
oscaralejandrofrancosilva <oskr.88@hotmail.com>
2023-01-18 23:01:04 -0600

    filter

--- components/organisms/filter/Filter.jsx
+++ components/organisms/filter/Filter.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import cx from 'classnames';
 
 import styles from './Filter.module.scss';
+import { FaAngleDown, FaAngleRight } from "react-icons/fa";
 
 function Filter() {
   const searchPlaceHolder = "EX: Carperter; sky Diver; Electrisian; Locksmith";
+  const resultNumber = 500;
   return (
     <div className={styles.filter}>
       <div className={cx(styles.row, styles.textboxContainer)}>
         <input className={styles.textbox} type="search" placeholder={searchPlaceHolder}></input>
       </div>
-      <div className={styles.row}>
-        Results: 500
+      <div className={cx(styles.row, styles.secondRow, styles.center)}>
+      <div className={styles.center}>Results <FaAngleRight /> {resultNumber}</div>
+      <div className={styles.center}>Filter <FaAngleDown /></div>
       </div>
     </div>
   )
